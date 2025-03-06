883d0f30fb1029de48c78a7134c5361aea19aae6
Garo Keshishian <garo.keshish@gmail.com>
2025-03-06 16:41:50 +0100

    init auth service in facade

--- tests/test_sdk_facade.py
+++ tests/test_sdk_facade.py
@@ -1,10 +1,9 @@
 from unittest.mock import patch
 from walacor_python_sdk.sdk_facade import SDKFacade
 
-
 def test_sdk_facade_auth_service_lazy_loading():
-    with patch("walacor_python_sdk.authentication.auth_service.AuthService", autospec= True) as mock_auth_service:
-        facade = SDKFacade()
+    with patch("walacor_python_sdk.authentication.auth_service.AuthService", autospec=True) as mock_auth_service:
+        facade = SDKFacade(auth_service_cls=mock_auth_service)
 
         assert facade._auth is None
         _ = facade.auth 
--- walacor_python_sdk/sdk_facade.py
+++ walacor_python_sdk/sdk_facade.py
@@ -1,2 +1,12 @@
+from walacor_python_sdk.authentication.auth_service import AuthService
+
 class SDKFacade:
-    pass
+    def __init__(self, auth_service_cls=AuthService): 
+        self._auth = None
+        self.auth_service_cls = auth_service_cls
+
+    @property
+    def auth(self) -> AuthService:
+        if self._auth is None:
+            self._auth = self.auth_service_cls() 
+        return self._auth
