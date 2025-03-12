9a01fb48d705994b89653c200b0e08fe598f52d1
Garo Keshishian <garo.keshish@gmail.com>
2025-03-12 23:59:13 +0100

    test passed

--- walacor_python_sdk/main.py
+++ walacor_python_sdk/main.py
@@ -1,8 +1,12 @@
-from w_client import W_Client
-from sdk_facade import SDKFacade
+from walacor_python_sdk.sdk_facade import SDKFacade
+from walacor_python_sdk.w_client import W_Client
+
 
 class Main:
     def greeting(self, name: str) -> str:
         return f"Hello {name}"
 
 
+client = W_Client("http://44.203.135.89/api", "Admin", "GreenDoor99")
+instance1 = SDKFacade(client)
+instance1.auth.login()
