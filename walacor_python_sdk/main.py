a6cc47d96cfb7b14af6ed2893056f8b05b6bcb72
Garo Keshishian <garo.keshish@gmail.com>
2025-03-13 00:02:22 +0100

    remove main

--- walacor_python_sdk/main.py
+++ walacor_python_sdk/main.py
@@ -5,8 +5,3 @@ from walacor_python_sdk.w_client import W_Client
 class Main:
     def greeting(self, name: str) -> str:
         return f"Hello {name}"
-
-
-client = W_Client("http://44.203.135.89/api", "Admin", "GreenDoor99")
-instance1 = SDKFacade(client)
-instance1.auth.login()
