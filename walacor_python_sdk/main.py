82ad6cbd687f351949acf69e6b8ca86eeef11f3a
Garo Keshishian <garo.keshish@gmail.com>
2025-03-13 00:26:59 +0100

    fix lint

--- walacor_python_sdk/main.py
+++ walacor_python_sdk/main.py
@@ -1,7 +1,3 @@
-from walacor_python_sdk.sdk_facade import SDKFacade
-from walacor_python_sdk.w_client import W_Client
-
-
 class Main:
     def greeting(self, name: str) -> str:
         return f"Hello {name}"
